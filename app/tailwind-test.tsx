8bade2d4df58e7400f4ef786d6bbcee3df60deaf
EC2 Default User <ec2-user@ip-172-31-81-182.ec2.internal>
2025-04-21 17:10:10 +0000

    🛠 Fix PostCSS config for Tailwind v3 + Amplify build

--- app/tailwind-test.tsx
+++
@@ -1,8 +0,0 @@
-// app/tailwind-test.tsx
-export default function TailwindTest() {
-  return (
-    <div className="bg-black text-white p-6 rounded-md shadow-md text-xl">
-      Tailwind is working
-    </div>
-  );
-}
