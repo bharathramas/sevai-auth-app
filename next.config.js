c1fcbb0468a0e611c57fb6b9bded10b4f1a39d08
EC2 Default User <ec2-user@ip-172-31-81-182.ec2.internal>
2025-04-22 06:48:16 +0000

    Expose NEXTAUTH_SECRET and Cognito env vars to Next runtime

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  env: {
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    COGNITO_CLIENT_ID: process.env.COGNITO_CLIENT_ID,
+    COGNITO_CLIENT_SECRET: process.env.COGNITO_CLIENT_SECRET,
+  },
 };
 
 module.exports = nextConfig;
