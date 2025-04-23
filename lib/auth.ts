baa083967d11263540f9d109335957106138bb47
EC2 Default User <ec2-user@ip-172-31-81-182.ec2.internal>
2025-04-23 02:17:58 +0000

    🔧 Fix: lib auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,3 +1,4 @@
 // lib/auth.ts
-import { authOptions as nextAuthOptions } from "@/pages/api/auth/[...nextauth]";
+//import { authOptions as nextAuthOptions } from "@/pages/api/auth/[...nextauth]";
+import { authOptions as nextAuthOptions } from "@/lib/authOptions";
 export const authOptions = nextAuthOptions;
